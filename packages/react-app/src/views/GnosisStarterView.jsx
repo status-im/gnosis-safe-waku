61b881a45a1a38d80f960a9dc377dd9161c05bfb
Soptq <32592090+soptq@users.noreply.github.com>
2022-03-31 12:17:54 +0800

    fix: remove redundant fields

--- packages/react-app/src/views/GnosisStarterView.jsx
+++ packages/react-app/src/views/GnosisStarterView.jsx
@@ -301,112 +301,6 @@ export default function GnosisStarterView({
     }
   },3333);
 
-  // const [ walletConnectUrl, setWalletConnectUrl ] = useState()
-  // const [ connected, setConnected ] = useState()
-
-  // useEffect(()=>{
-  //   //walletConnectUrl
-  //   if(walletConnectUrl){
-  //     const connector = new WalletConnect(
-  //       {
-  //         // Required
-  //         uri: walletConnectUrl,
-  //         // Required
-  //         clientMeta: {
-  //           description: "Gnosis Safe Starter Kit",
-  //           url: "https://github.com/austintgriffith/scaffold-eth/tree/gnosis-starter-kit",
-  //           icons: ["http://s3.amazonaws.com/pix.iemoji.com/images/emoji/apple/ios-12/256/owl.png"],
-  //           name: "Gnosis Safe Starter Kit",
-  //         },
-  //       }/*,
-  //       {
-  //         // Optional
-  //         url: "<YOUR_PUSH_SERVER_URL>",
-  //         type: "fcm",
-  //         token: token,
-  //         peerMeta: true,
-  //         language: language,
-  //       }*/
-  //     );
-  //
-  //     // Subscribe to session requests
-  //     connector.on("session_request", (error, payload) => {
-  //       if (error) {
-  //         throw error;
-  //       }
-  //
-  //       console.log("SESSION REQUEST")
-  //       // Handle Session Request
-  //
-  //       connector.approveSession({
-  //         accounts: [                 // required
-  //           safeAddress
-  //         ],
-  //         chainId: targetNetwork.chainId               // required
-  //       })
-  //
-  //       setConnected(true)
-  //
-  //
-  //       /* payload:
-  //       {
-  //         id: 1,
-  //         jsonrpc: '2.0'.
-  //         method: 'session_request',
-  //         params: [{
-  //           peerId: '15d8b6a3-15bd-493e-9358-111e3a4e6ee4',
-  //           peerMeta: {
-  //             name: "WalletConnect Example",
-  //             description: "Try out WalletConnect v1.0",
-  //             icons: ["https://example.walletconnect.org/favicon.ico"],
-  //             url: "https://example.walletconnect.org"
-  //           }
-  //         }]
-  //       }
-  //       */
-  //     });
-  //
-  //     // Subscribe to call requests
-  //     connector.on("call_request", (error, payload) => {
-  //       if (error) {
-  //         throw error;
-  //       }
-  //
-  //       console.log("REQUEST PERMISSION TO:",payload,payload.params[0])
-  //       // Handle Call Request
-  //       console.log("SETTING TO",payload.params[0].to)
-  //       setTo(payload.params[0].to)
-  //       setData(payload.params[0].data?payload.params[0].data:"0x0000")
-  //       setValue(payload.params[0].value)
-  //       /* payload:
-  //       {
-  //         id: 1,
-  //         jsonrpc: '2.0'.
-  //         method: 'eth_sign',
-  //         params: [
-  //           "0xbc28ea04101f03ea7a94c1379bc3ab32e65e62d3",
-  //           "My email is john@doe.com - 1537836206101"
-  //         ]
-  //       }
-  //       */
-  //       /*connector.approveRequest({
-  //         id: payload.id,
-  //         result: "0x41791102999c339c844880b23950704cc43aa840f3739e365323cda4dfa89e7a"
-  //       });*/
-  //
-  //     });
-  //
-  //     connector.on("disconnect", (error, payload) => {
-  //       if (error) {
-  //         throw error;
-  //       }
-  //       console.log("disconnect")
-  //
-  //       // Delete connector
-  //     });
-  //   }
-  // },[ walletConnectUrl ])
-
 
   let safeInfo
   if(safeAddress){
@@ -496,33 +390,9 @@ export default function GnosisStarterView({
               price={price}
               placeholder="Enter Tx Value"
               value={value}
-              /*
-              onChange={v => {
-                v = v && v.toString && v.toString()
-                if(v){
-                  const valueResult = ethers.utils.parseEther(""+v.replace(/\D/g, ""))
-                  setValue(valueResult);
-                }
-
-              }}*/
               onChange={setValue}
             />
           </div>
-          <div style={{ padding: 4 }}>
-            <Input placeholder="Enter Selector i.e add(uint, uint)"
-              onChange={async (e) => {
-                setSelector(e.target.value)
-              }}
-            />
-          </div>
-          <div style={{ padding: 4 }}>
-            <Input placeholder="Enter arguments separated by ,"
-              onChange={async (e) => {
-                setParams(e.target.value.split(','))
-              }}
-            />
-          </div>
-          {data?data:""}
           <Button
             style={{ marginTop: 8 }}
             onClick={async () => {
